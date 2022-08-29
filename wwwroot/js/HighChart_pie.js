bf299a3532b977e9cb6f1d6b9584e07d81001798
Bcepnez <senju_mr@hotmail.com>
2022-08-30 00:13:54 +0700

    customize 1 Chart color and expanded component in row

--- wwwroot/js/HighChart_pie.js
+++ wwwroot/js/HighChart_pie.js
@@ -20,7 +20,14 @@ function CreateWaterStation_Highchart(data) {
             }
         },
         series: [{
-            colors: ['rgb(240, 75, 79)', 'rgb(97, 121, 185)', 'rgb(50, 191, 113)',  'rgb(248, 199, 68)','rgb(226, 153, 85)','rgb(224,224,224)'],
+            colors: [
+                'rgb(240, 75, 79)',
+                'rgb(97, 121, 185)',
+                'rgb(50, 191, 113)',
+                'rgb(248, 199, 68)',
+                'rgb(226, 153, 85)',
+                'rgb(224, 224, 224)'
+            ],
             name: data.name,
             dataLabels: data.dataLabels,
             data: data.data
@@ -50,6 +57,13 @@ function CreateRainLevel_Highchart(data) {
             }
         },
         series: [{
+            colors: [
+                'rgb(218, 53, 46)',
+                'rgb(254, 138, 4)',
+                'rgb(156, 238, 178)',
+                'rgb(185, 218, 251)',
+                'rgb(220, 220, 220)'
+            ],
             name: data.name,
             dataLabels: data.dataLabels,
             data: data.data
